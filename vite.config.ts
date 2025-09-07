609ea047cedef89219eefdc838392c50d4f6e50e
Pouria Neishabourian <pouria.neyshabourian@swai.ai>
2025-09-07 16:59:27 +0330

    feat: add PWA support with manifest and icons for Note App

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,79 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup.js",
-  },
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: "Note App",
+        name: "Note App",
+        display: "standalone",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/320.png",
+            sizes: "320x320",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        id: "/",
+        start_url: "/",
+        background_color: "#3367D6",
+        scope: "/",
+        theme_color: "#3367D6",
+        shortcuts: [
+          {
+            name: "Note App",
+            short_name: "Note App",
+            description: "View Note App",
+            url: "/",
+            icons: [{ src: "/web-app-manifest-512x512.png", sizes: "512x512" }],
+          },
+          {
+            name: "Note App",
+            short_name: "Note App",
+            description: "View Note App",
+            url: "/",
+            icons: [{ src: "/web-app-manifest-192x192.png", sizes: "192x192" }],
+          },
+          {
+            name: "Note App",
+            short_name: "Note App",
+            description: "View Note App",
+            url: "/",
+            icons: [{ src: "/320.png", sizes: "320x320" }],
+          },
+        ],
+        description: "Note App Information",
+        screenshots: [
+          {
+            src: "/320.png",
+            type: "image/png",
+            sizes: "192x192",
+            form_factor: "narrow",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+            form_factor: "wide",
+          },
+        ],
+      },
+    }),
+  ],
+});
